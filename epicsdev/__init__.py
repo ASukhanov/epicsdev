1d5619444546a3600a487390c99c692903384de3
Sukhanov, Andrei <sukhanov@bnl.gov>
2026-03-05 15:36:02 -0500

    empty __init__.py

--- epicsdev/__init__.py
+++
@@ -1,8 +0,0 @@
-"""epicsdev package.
-
-Main runnable modules:
-- epicsdev.epicsdev : generic/demo PVAccess server framework
-- epicsdev.multiadc : multi-channel waveform generator example
-- epicsdev.putlog   : writable `dump` PV that appends incoming text to a file
-"""
-
