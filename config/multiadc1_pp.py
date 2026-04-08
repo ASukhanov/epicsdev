7c33a1373e8cd9fca907d3771845e0c66c1922e6
Sukhanov, Andrei <sukhanov@bnl.gov>
2026-04-08 09:42:22 -0400

    release 3.2.1

--- config/multiadc1_pp.py
+++
@@ -1,6 +0,0 @@
-"""Pypet definition for device multiadc1"""
-import multiadc_pp as module
-
-def PyPage(**_):
-    return  module.PyPage(instance='multiadc1:', title='multiadc1',
-        channels=6)
